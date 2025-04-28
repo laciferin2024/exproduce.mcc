47630f1705ea0e3cb451c0d606d290214aa14798
Nasfame <laciferin2024@protonmail.com>
2025-04-29 02:00:24 +0400

    hardhat: config updated

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,22 +1,24 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-import "@nomicfoundation/hardhat-ethers";
-import "@nomicfoundation/hardhat-chai-matchers";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-import "@nomicfoundation/hardhat-verify";
-import "hardhat-deploy";
-import "hardhat-deploy-ethers";
-import { HardhatUserConfig } from "hardhat/types/config";
+import * as dotenv from "dotenv"
+dotenv.config()
+import "@nomicfoundation/hardhat-ethers"
+import "@nomicfoundation/hardhat-chai-matchers"
+import "@typechain/hardhat"
+import "hardhat-gas-reporter"
+import "solidity-coverage"
+import "@nomicfoundation/hardhat-verify"
+import "hardhat-deploy"
+import "hardhat-deploy-ethers"
+import { HardhatUserConfig } from "hardhat/types/config"
 
 // If not set, it uses ours Alchemy's default API key.
 // You can get your own at https://dashboard.alchemyapi.io
-const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
+const providerApiKey =
+  process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF"
 // If not set, it uses the hardhat account 0 private key.
-const deployerPrivateKey = process.env.PRIVATE_KEY || "";
+const deployerPrivateKey = process.env.PRIVATE_KEY || ""
 // If not set, it uses ours Etherscan default API key.
-const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
+const etherscanApiKey =
+  process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW"
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -44,18 +46,21 @@ const config: HardhatUserConfig = {
     // View the networks that are pre-configured.
     // If the network you are looking for is not here you can add new network settings
     hardhat: {
-      // forking: {
-      //   url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
-      //   enabled: process.env.MAINNET_FORKING_ENABLED === "true",
-      // },
-      // chainId: 1337,
+      forking: {
+        url: `https://polygon-amoy.g.alchemy.com/v2/${providerApiKey}`,
+        enabled: true,
+        usdc: "0x6EEBe75caf9c579B3FBA9030760B84050283b50a",
+        // enabled: process.env.MAINNET_FORKING_ENABLED === "true",
+      },
+      chainId: 1337,
       accounts: [
         {
           privateKey: deployerPrivateKey,
           balance: `${1000000000000000000000000n}`,
         },
         {
-          privateKey: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+          privateKey:
+            "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
           balance: `${1000000000000000000000000n}`,
         },
       ],
@@ -67,7 +72,7 @@ const config: HardhatUserConfig = {
     amoy: {
       url: `https://polygon-amoy.g.alchemy.com/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
-      usdc: "0x6EEBe75caf9c579B3FBA9030760B84050283b50a"
+      usdc: "0x6EEBe75caf9c579B3FBA9030760B84050283b50a",
     },
     baseSepolia: {
       url: "https://sepolia.base.org",
@@ -91,6 +96,6 @@ const config: HardhatUserConfig = {
   sourcify: {
     enabled: false,
   },
-};
+}
 
-export default config;
+export default config
