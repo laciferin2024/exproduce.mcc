d6024e88d806661c920ec086962bc93b31146693
Nasfame <laciferin2024@protonmail.com>
2025-04-29 01:34:29 +0400

    import all: code

---
+++ hardhat/deploy/01_deploy_token.ts
@@ -0,0 +1,48 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { Contract } from "ethers";
+
+/**
+ * Deploys a contract named "YourContract" using the deployer account and
+ * constructor arguments set to the deployer address
+ *
+ * @param hre HardhatRuntimeEnvironment object.
+ */
+const deployToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+    On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
+
+    When deploying to live networks (e.g `yarn deploy --network sepolia`), the deployer account
+    should have sufficient balance to pay for the gas fees for contract creation.
+
+    You can generate a random account with `yarn generate` which will fill DEPLOYER_PRIVATE_KEY
+    with a random private key in the .env file (then used on hardhat.config.ts)
+    You can run the `yarn account` command to check your balance in every network.
+  */
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  console.log("deployer:", deployer);
+
+  await deploy("MinaFountain", {
+    from: deployer,
+    // Contract constructor arguments
+    args: [],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+
+  // Get the deployed contract to interact with it after deploying.
+  const token = await hre.ethers.getContract<Contract>("MinaFountain", deployer);
+  console.log("👋 Initial fountain:", await token.initialSupply());
+
+  // await token.transfer("0x0", 100);
+};
+
+deployToken.id = "deployToken";
+
+export default deployToken;
+
+module.exports.tags = ["all", "deployMinaFountain", "token"];
