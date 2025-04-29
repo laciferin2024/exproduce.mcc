8c2af5ff8169949cd164467e4e55b9817e51e27d
Nasfame <laciferin2024@protonmail.com>
2025-04-29 05:52:41 +0400

    add: sw

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+// This is a basic service worker file
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Fetching:', event.request.url);
+});
